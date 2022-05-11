e78425c552e6970dd0b23ad083e92108a131a5e2
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-05-11 18:00:03 -0300

    updated pages

--- src/data/DataScienceData.js
+++ src/data/DataScienceData.js
@@ -2,9 +2,9 @@
 export const Blogs = [
     {
         id:1,
-        name:"Analyze and classify public domain EEG(ML) signals provided by physionet.org",
+        name:"Analyze and classify public domain EEG(ML) signals",
         tags:["EEG","python","machine learning","neuroscience"],
-        date:"13 May, 2021",
+        date:"22 May, 2020",
         imgSrc:"https://camo.githubusercontent.com/aa6c527b5fd69d3e98fc7d821f3c3cf94340ee0c0b99959b96e8a76b7f4810d2/68747470733a2f2f63646e2e746563686e6f6c6f67796e6574776f726b732e636f6d2f746e2f696d616765732f7468756d62732f776562702f3634305f3336302f66697273742d657665722d6e6f6e2d696e7661736976652d627261696e2d636f6d70757465722d696e746572666163652d646576656c6f7065642d3332303934312e776562703f763d3130323430343736",
         link:"https://github.com/DavidSilveraGabriel/EEG-classification"
     },
