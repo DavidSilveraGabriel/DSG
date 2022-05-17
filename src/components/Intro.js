a8cf0a0e302587ca4925fb8807551ea8c045e7d4
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-05-17 12:02:29 -0300

    update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -72,7 +72,7 @@ const Intro = () => {
                 <Text>
                     <h1>Hi!!</h1>
                     <h3>I'm David Silvera</h3>
-                    <h6>I code and provide data driven solutions for any kind of business problem</h6>
+                    <h6>I provide data driven solutions for any kind of business problem</h6>
                 </Text>
             </SubBox>
             <SubBox>
