26497f865e31bdf7a655d4a9a5ce4eb821ba3f8f
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-05-21 16:26:25 -0300

    update

--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -1,4 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import matter from "gray-matter";
-import {serialize} from 'next-mdx-remote/serialize'
+import {serialize} from 'next-mdx-remote/serialize';
+
+const root = process.cwd();
+
+export const getFiles = () => {
+    fs.readFileSync(path.join(root,'src/data/blogs'))
+};
+
+export const getFileBySlug = ({ slug }) => {}
+
+export const getAllFilesMetadata = () => {};
