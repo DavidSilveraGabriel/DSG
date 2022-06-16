4199cd479c3643d9262e6a2057f87e6b0ff4e126
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-06-16 14:30:31 -0300

    updates

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -74,7 +74,7 @@ const BlogPage = () => {
                     </Grid>
                     
                 </Center>
-                <BigTitle text="BLOG" top="5rem" left="5rem" />
+                <BigTitle text="BLOG" top="3rem" left="3rem" />
             </Container>
         </MainContainer>
     );
