996f94b0f39420433d72fe09b9c254b9cbd592ea
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-07-02 10:26:33 -0300

    cv

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,7 +6,6 @@ import { NavLink } from "react-router-dom";
 
 import LogoComponent from '../subComponents/LogoComponent';
 import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
 import ParticleComponent from '../subComponents/ParticleComponent';
 import BigTitle from '../subComponents/BigTitlte'
 
