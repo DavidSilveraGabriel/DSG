ce731199e7732db2ce978f18e7e324ad8a77dcd8
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-07-13 23:41:35 -0300

    updates

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -41,7 +41,7 @@ const Title = styled.h3`
 color: inherit;
 padding: 0.5rem 0;
 padding-top: 1rem;
-font-family: 'Karla', sans-serif;
+font-family:'Hanalei Fill','Karla', sans-serif;
 font-weight: 700;
 border-bottom: 1px solid ${props => props.theme.text};
 
