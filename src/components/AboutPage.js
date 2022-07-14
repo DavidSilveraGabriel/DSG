46562c3d141825d4812dae31df6f1eba5e7a3841
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-07-13 23:54:02 -0300

    updates

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -107,6 +107,11 @@ const AboutPage = () => {
             EEG classifier for move an arduino robot with the mind in just 3 month, 
             also computer vision, creating an app that controls the mouse estimating 
             the pose of the head and eyes, and today in education as a tutor of data science.</p>
+            <p>I'm an Argentine Self-taught Data Scientist with +2 years of experience, 
+            worked as a freelancer in the areas of neuroscience, developing an end-to-end 
+            EEG classifier for move an arduino robot with the mind in just 3 month, 
+            also computer vision, creating an app that controls the mouse estimating 
+            the pose of the head and eyes, and today in education as a tutor of data science.</p>
 
             <br/> <br/>
             I believe that the future is closely linked to web 3.0, so my goal will be to move to that risky side,
