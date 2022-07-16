9e4c4f27665856ac96bb9128dde0c0d8eb25f3c1
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-07-15 21:51:36 -0300

    updates

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -7,15 +7,7 @@ export const Blogs = [
         date:"13 May, 2021",
         imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
         link:"github_copilot.md"
-    },
-    {
-        id:2,
-        name:"How to create React Hamburger Menu using Styled-Components",
-        tags:["react","styled-components"],
-        date:"5 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
-    },
+    }
     
     
 ] 
