8abd8fe1e5e656045b4481b3b52ce651ce737f45
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-07-16 11:51:45 -0300

    updates

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -16,7 +16,14 @@ export const Blogs = [
         imgSrc:"https://miro.medium.com/max/929/0*5WRf1PlvjI60ExYY.png",
         link:"https://medium.com/towards-data-science/mlops-in-10-minutes-165c746a9b8e"
     },
-    
+    {
+        id:3,
+        name:"6 Sklearn Mistakes That Silently Tell You Are Rookie",
+        tags:["Sklearn","MachineLearning","DataScience"],
+        date:"8 Apr, 2022",
+        imgSrc:"https://miro.medium.com/max/945/1*DYFK5voaJ4o5Uz99NvVX-g.jpeg",
+        link:"https://medium.com/@ibexorigin/6-sklearn-mistakes-that-silently-tell-you-are-rookie-f1fe44779a4d"
+    },
 
     
 ] 
