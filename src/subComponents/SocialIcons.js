4270f5791c066720941e68c5fb4cadb5ec13d272
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-08-09 19:05:14 -0300

    icons updates

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -53,7 +53,7 @@ const SocialIcons = (props) => {
             transition={{type:'spring', duration:1, delay:1.4}}
             >
                 <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://www.kaggle.com/davidsilvera"}}>
-                    <FaKaggle width={40} height={40} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
+                    <FaKaggle width={100} height={100} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
                 </NavLink>
             </motion.div>
             <motion.div
@@ -62,7 +62,7 @@ const SocialIcons = (props) => {
             transition={{type:'spring', duration:1, delay:1.6}}
             >
                 <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://www.youtube.com/channel/UCK_nb91XsdzJHr4isq3d1ZA"}}>
-                    <FaYoutube width={40} height={40} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
+                    <FaYoutube width={100} height={100} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
                 </NavLink>
             </motion.div>
             
