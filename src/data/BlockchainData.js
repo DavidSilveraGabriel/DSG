4f657a0b8199a75f4e500c69cb66e91267d6fbad
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-08-16 18:13:04 -0300

    updates

--- src/data/BlockchainData.js
+++ src/data/BlockchainData.js
@@ -13,7 +13,7 @@ export const Blogs = [
         name:"The blockchain developer resources",
         tags:["blockchain","Metamask","resources"],
         date:"16 Aug 2022",
-        imgSrc:"https://blog.formaciongerencial.com/wp-content/uploads/2021/11/Web3.jpg",
+        imgSrc:"https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2022/01/web3-2585263.jpg?itok=zHqw-e1i",
         link:"https://github.com/DavidSilveraGabriel/The-blockchain-dev-resources"
     }
   
